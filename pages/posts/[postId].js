c0a8a001ed5e73554829165c118c93df82e98414
Nancy Kataria <katarianancy8@gmail.com>
2022-02-23 22:27:55 +0530

    changing paths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -28,7 +28,38 @@ export async function getStaticPaths() {
   });
 
   return {
-    paths: paths,
+    paths: [
+      {
+        params: { postId : '1'},
+      },  
+      {
+        params: { postId : '2'},
+      },  
+      {
+        params: { postId : '3'},
+      },  
+      {
+        params: { postId : '4'},
+      },  
+      {
+        params: { postId : '5'},
+      },  
+      {
+        params: { postId : '6'},
+      },  
+      {
+        params: { postId : '7'},
+      },  
+      {
+        params: { postId : '8'},
+      },  
+      {
+        params: { postId : '9'},
+      },  
+      {
+        params: { postId : '10'},
+      },  
+    ],
     fallback: false,
   };
 }
